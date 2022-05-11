c0f48390c95adc18061df697e1736291b4266c61
Túlio Teodoro <tulioredes@gmail.com>
2022-05-11 15:08:13 -0300

    feat: ajustando pasta de academia

---
+++ gym/gym.py
@@ -0,0 +1,12 @@
+class Gym:
+
+    def __init__(self,nome,cnpj,email,telefone):
+        self.nome=nome
+        self.cnpj=cnpj
+        self.email=email
+        self.telefone=telefone
+
+
+gym_list=[
+    Gym("Academia Fique Fino","91.909.512/0001-20","academia_fiquefino@gmail.com","32201491"),
+]
